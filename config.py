a56bff1212129bee95951d64a1fc985da863ea5f
yundaji <97157839+yundaji@users.noreply.github.com>
2026-06-22 19:49:18 +0800

    Update CSV_URL to point to a specific Google Sheets

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 # Google CSV
-CSV_URL = "https://docs.google.com/"
+CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSAXrKFcPS_SXpMl9wdevqssrU93K49p7ucsERuAWieWSoMnNzociSaxTagLvoRCcTIghACfZRzMPpb/pub?output=csv"
 
 # 文章来源
 START_URL = "https://www.mhwmm.com/miandianxinwen/81512.html"
